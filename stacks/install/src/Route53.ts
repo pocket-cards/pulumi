1da2b697eb04df4c9e321e5caf818121fb40058a
wwalpha <wwalpha@gmail.com>
2020-05-14 06:08:00 +0000

    update

--- stacks/install/src/Route53.ts
+++ stacks/install/src/Route53.ts
@@ -10,7 +10,7 @@ export default (): Install.Route53Outputs => {
       comment: Consts.DOMAIN_NAME(),
       forceDestroy: false,
     },
-    { protect: true }
+    { protect: true, ignoreChanges: ['forceDestroy'] }
   );
 
   return {
