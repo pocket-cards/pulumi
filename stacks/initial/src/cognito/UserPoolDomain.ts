8c971d78d3a230cd2731f8f07d80b9a7e65c65ed
wwalpha <wwalpha@gmail.com>
2020-07-18 01:42:13 +0000

    bugfix

--- stacks/initial/src/cognito/UserPoolDomain.ts
+++ stacks/initial/src/cognito/UserPoolDomain.ts
@@ -2,7 +2,7 @@ import { cognito } from '@pulumi/aws';
 import { Envs } from '../../../consts';
 
 export default (pool: cognito.UserPool) => {
-  const domainName = Envs.IS_DEV ? 'card-dev' : 'card';
+  const domainName = Envs.IS_DEV() ? 'card-dev' : 'card';
 
   const domain = new cognito.UserPoolDomain('cognito.userpool.domain', {
     domain: domainName,
